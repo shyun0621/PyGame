18f4684b01dcec9c23f7834992a05d4f41efab97
SangHyun_Lucas <sanghyun.yun@geappliances.com>
2022-12-07 13:25:34 +0900

    * Fixed blinking issue for pygame 2.0.0
    * Modified some items and images

--- pygame/app.py
+++ pygame/app.py
@@ -58,12 +58,12 @@ class CycleObject(pygame.sprite.Sprite):
                         self.clicked = not self.clicked
                         if self.clicked:
                             clickedMenuIndex = self.index
-                            clickedItemIndex = -1
+                            clickedItemIndex = 0
                             dialog_box.reset(hard=True)
                             dialog_box.set_text(dialog_text[FINAL_STEP + clickedMenuIndex + 1])
                         elif not self.clicked:
                             clickedMenuIndex = -1
-                            clickedItemIndex = -1
+                            clickedItemIndex = 0
                             dialog_box.reset(hard=True)
                             dialog_box.set_text(dialog_text[10])
 
@@ -89,24 +89,21 @@ class InventorySlotObject(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
         self.clicked = False
+        self.index = index
+
+    def update(self, event_list):
         text = "0"
-        if index == 0:
-            text = "1"
-        elif index == 1:
-            text = "5"
-        elif index == 2:
-            text = "10"
-        elif index == 3:
-            text = "30"
-        elif index == 4:
-            text = "60"
+        global clickedMenuIndex
+        if clickedMenuIndex == 0:
+            text = itemText[0][self.index]
+        else:
+            text = itemText[1][self.index]
 
         self.textSurf = myFont.render(text, True, (255, 255, 255))
         W = self.textSurf.get_width()
         H = self.textSurf.get_height()
         self.image.blit(self.textSurf, [0, 0])
 
-    def update(self, event_list):
         for event in event_list:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.rect.collidepoint(event.pos):
@@ -118,21 +115,25 @@ class InventorySlotObject(pygame.sprite.Sprite):
                     clickedItemIndex = self.index
 
                     selectedItem[clickedMenuIndex] = clickedItemIndex
-                    if self.index == -1:
+                    if self.index == 0:
                         cycle[clickedMenuIndex] = 0
-                    elif self.index == 0:
-                        cycle[clickedMenuIndex] += 1
                     elif self.index == 1:
-                        cycle[clickedMenuIndex] += 5
+                        cycle[clickedMenuIndex] += 1
                     elif self.index == 2:
-                        cycle[clickedMenuIndex] += 10
+                        cycle[clickedMenuIndex] += 2
                     elif self.index == 3:
-                        cycle[clickedMenuIndex] += 30
+                        cycle[clickedMenuIndex] += 4
                     elif self.index == 4:
-                        cycle[clickedMenuIndex] += 60
+                        cycle[clickedMenuIndex] += 8
 
-                    if cycle[clickedMenuIndex] > 60:
-                        cycle[clickedMenuIndex] = 60
+                    if clickedMenuIndex == 0:
+                        if cycle[clickedMenuIndex] > 8:
+                            cycle[clickedMenuIndex] = 8
+                    else:
+                        if cycle[clickedMenuIndex] > 40:
+                            cycle[clickedMenuIndex] = 40
+
+                    
 
 class BarObject(pygame.sprite.Sprite):
     def __init__(self):
@@ -143,34 +144,47 @@ class BarObject(pygame.sprite.Sprite):
 
     def render(self, display):
         display.blit(self.image, self.rect)
+        global clickedMenuIndex
         global selectedItem
         global isAllSet
-        for idx in range(len(selectedItem)):
-            if selectedItem[idx] >= 0:
-                if idx == 0:
-                    pygame.draw.rect(display, PUPPLE, (DEFAULT_BAR_X, DEFAULT_BAR_Y - 1, DEFAULT_BAR_WIDTH * cycle[idx], DEFAULT_BAR_HEIGHT), 0)
-                    title= myFont.render(str(cycle[idx]), True, BLACK)
-                    screen.blit(title, [1450, DEFAULT_BAR_Y - 1])
-                elif idx == 1:
-                    pygame.draw.rect(display, GREEN, (DEFAULT_BAR_X, DEFAULT_BAR_Y + 52, DEFAULT_BAR_WIDTH * cycle[idx], DEFAULT_BAR_HEIGHT), 0)
-                    title= myFont.render(str(cycle[idx]), True, BLACK)
-                    screen.blit(title, [1450, DEFAULT_BAR_Y + 52])
-                elif idx == 2:
-                    pygame.draw.rect(display, BLUE, (DEFAULT_BAR_X, DEFAULT_BAR_Y + 105, DEFAULT_BAR_WIDTH * cycle[idx], DEFAULT_BAR_HEIGHT), 0)
-                    title= myFont.render(str(cycle[idx]), True, BLACK)
-                    screen.blit(title, [1450, DEFAULT_BAR_Y + 105])
-                elif idx == 3:
-                    pygame.draw.rect(display, RED, (DEFAULT_BAR_X, DEFAULT_BAR_Y + 160, DEFAULT_BAR_WIDTH * cycle[idx], DEFAULT_BAR_HEIGHT), 0)
-                    title= myFont.render(str(cycle[idx]), True, BLACK)
-                    screen.blit(title, [1450, DEFAULT_BAR_Y + 159])
-                elif idx == 4:
-                    pygame.draw.rect(display, YELLOW, (DEFAULT_BAR_X, DEFAULT_BAR_Y + 213, DEFAULT_BAR_WIDTH * cycle[idx], DEFAULT_BAR_HEIGHT), 0)
-                    title= myFont.render(str(cycle[idx]), True, BLACK)
-                    screen.blit(title, [1450, DEFAULT_BAR_Y + 214])
+        global itemText
+        color = YELLOW
+        offsetH = 0
+        offsetW = 0
 
+        pygame.draw.rect(display, YELLOW, (DEFAULT_BAR_X, DEFAULT_BAR_Y + 213, DEFAULT_BAR_WIDTH * 60, DEFAULT_BAR_HEIGHT), 0)
+
+        for idx in range(len(selectedItem)):         
+            if selectedItem[idx] >= 0:           
+                if clickedMenuIndex == 0:
+                    title = myFont.render(str(cycle[idx]), True, BLACK)
+                else:
+                    title = myFont.render(str(cycle[idx]), True, BLACK)
+                    
+                if idx == 0:
+                    offsetH = 0
+                    offsetW = cycle[idx] * 60 / 8
+                    color = PUPPLE
+                elif idx == 1:
+                    offsetH = 53
+                    offsetW = cycle[idx] * 60 / 40
+                    color = GREEN
+                elif idx == 2:
+                    offsetH = 106
+                    offsetW = cycle[idx] * 60 / 40
+                    color = BLUE
+                elif idx == 3:
+                    offsetH = 161
+                    offsetW = cycle[idx] * 60 / 40
+                    color = RED
+                    
+                pygame.draw.rect(display, color, (DEFAULT_BAR_X, DEFAULT_BAR_Y + offsetH, DEFAULT_BAR_WIDTH * offsetW, DEFAULT_BAR_HEIGHT), 0)
+                screen.blit(title, [1450, DEFAULT_BAR_Y + offsetH])    
+
+                        
 
 DEFAULT_BAR_X = 1391
-DEFAULT_BAR_Y = 769
+DEFAULT_BAR_Y = 768
 DEFAULT_BAR_WIDTH = 7.4
 DEFAULT_BAR_HEIGHT = 34
 
@@ -190,54 +204,41 @@ item_group.add(CycleObject(10 + cycleItem_width / 2, 30, 'images/fill.png', 0))
 item_group.add(CycleObject(100 + cycleItem_width / 2, 30, 'images/wash.png', 1))
 item_group.add(CycleObject(190 + cycleItem_width / 2, 30, 'images/spin.png', 2))
 item_group.add(CycleObject(280 + cycleItem_width / 2, 30, 'images/rinse.png', 3))
-item_group.add(CycleObject(370 + cycleItem_width / 2, 30, 'images/drain.png', 4))
 
 fill_group = pygame.sprite.Group()
-fill_group.add(InventorySlotObject('images/potion_fill.png', (41, 162), -1))
-fill_group.add(InventorySlotObject('images/potion_fill.png', (139, 162), 0))
-fill_group.add(InventorySlotObject('images/potion_fill.png', (238, 162), 1))
-fill_group.add(InventorySlotObject('images/potion_fill.png', (41, 248), 2))
-fill_group.add(InventorySlotObject('images/potion_fill.png', (139, 248), 3))
-fill_group.add(InventorySlotObject('images/potion_fill.png', (238, 248), 4))
+fill_group.add(InventorySlotObject('images/potion_fill.png', ( 41, 162), 0))
+fill_group.add(InventorySlotObject('images/potion_fill.png', (139, 162), 1))
+fill_group.add(InventorySlotObject('images/potion_fill.png', (238, 162), 2))
+fill_group.add(InventorySlotObject('images/potion_fill.png', ( 41, 248), 3))
+fill_group.add(InventorySlotObject('images/potion_fill.png', (139, 248), 4))
 
 wash_group = pygame.sprite.Group()
-wash_group.add(InventorySlotObject('images/potion_wash.png', (41, 162), -1))
-wash_group.add(InventorySlotObject('images/potion_wash.png', (139, 162), 0))
-wash_group.add(InventorySlotObject('images/potion_wash.png', (238, 162), 1))
-wash_group.add(InventorySlotObject('images/potion_wash.png', (41, 248), 2))
-wash_group.add(InventorySlotObject('images/potion_wash.png', (139, 248), 3))
-wash_group.add(InventorySlotObject('images/potion_wash.png', (238, 248), 4))
+wash_group.add(InventorySlotObject('images/potion_wash.png', ( 41, 162), 0))
+wash_group.add(InventorySlotObject('images/potion_wash.png', (139, 162), 1))
+wash_group.add(InventorySlotObject('images/potion_wash.png', (238, 162), 2))
+wash_group.add(InventorySlotObject('images/potion_wash.png', ( 41, 248), 3))
+wash_group.add(InventorySlotObject('images/potion_wash.png', (139, 248), 4))
 
 spin_group = pygame.sprite.Group()
-spin_group.add(InventorySlotObject('images/potion_spin.png', (41, 162), -1))
-spin_group.add(InventorySlotObject('images/potion_spin.png', (139, 162), 0))
-spin_group.add(InventorySlotObject('images/potion_spin.png', (238, 162), 1))
-spin_group.add(InventorySlotObject('images/potion_spin.png', (41, 248), 2))
-spin_group.add(InventorySlotObject('images/potion_spin.png', (139, 248), 3))
-spin_group.add(InventorySlotObject('images/potion_spin.png', (238, 248), 4))
+spin_group.add(InventorySlotObject('images/potion_spin.png', ( 41, 162), 0))
+spin_group.add(InventorySlotObject('images/potion_spin.png', (139, 162), 1))
+spin_group.add(InventorySlotObject('images/potion_spin.png', (238, 162), 2))
+spin_group.add(InventorySlotObject('images/potion_spin.png', ( 41, 248), 3))
+spin_group.add(InventorySlotObject('images/potion_spin.png', (139, 248), 4))
 
 rinse_group = pygame.sprite.Group()
-rinse_group.add(InventorySlotObject('images/potion_rinse.png', (41, 162), -1))
-rinse_group.add(InventorySlotObject('images/potion_rinse.png', (139, 162), 0))
-rinse_group.add(InventorySlotObject('images/potion_rinse.png', (238, 162), 1))
-rinse_group.add(InventorySlotObject('images/potion_rinse.png', (41, 248), 2))
-rinse_group.add(InventorySlotObject('images/potion_rinse.png', (139, 248), 3))
-rinse_group.add(InventorySlotObject('images/potion_rinse.png', (238, 248), 4))
-
-drain_group = pygame.sprite.Group()
-drain_group.add(InventorySlotObject('images/potion_drain.png', (41, 162), -1))
-drain_group.add(InventorySlotObject('images/potion_drain.png', (139, 162), 0))
-drain_group.add(InventorySlotObject('images/potion_drain.png', (238, 162), 1))
-drain_group.add(InventorySlotObject('images/potion_drain.png', (41, 248), 2))
-drain_group.add(InventorySlotObject('images/potion_drain.png', (139, 248), 3))
-drain_group.add(InventorySlotObject('images/potion_drain.png', (238, 248), 4))
+rinse_group.add(InventorySlotObject('images/potion_rinse.png', ( 41, 162), 0))
+rinse_group.add(InventorySlotObject('images/potion_rinse.png', (139, 162), 1))
+rinse_group.add(InventorySlotObject('images/potion_rinse.png', (238, 162), 2))
+rinse_group.add(InventorySlotObject('images/potion_rinse.png', ( 41, 248), 3))
+rinse_group.add(InventorySlotObject('images/potion_rinse.png', (139, 248), 4))
 
 washer = WasherObject()
 inventory = InventoryObject()
 bar = BarObject()
 
 clickedMenuIndex = -1
-clickedItemIndex = -1
+clickedItemIndex = 0
 isAllSet = False
 dialog_step = 0
 FINAL_STEP = 4
@@ -246,12 +247,12 @@ dialog_text = [
     "Washer: Hi~ HyoLee",
     "HoyLee: How can I make a custom cycle?",
     "Washer: You can make it by clicking the items!!",
-    "Washer: Washer:Let's click first item",
-    "Washer: Select your Fill Item",
-    "Washer: Select your Wash Item",
-    "Washer: Select your Spin Item",
-    "Washer: Select your Rinse Item",
-    "Washer: Select your Drain Item",
+    "Washer: Let's click first item",
+    "Washer: Select your Fill Item (Gallon)",
+    "Washer: Select your Wash Item (Minutes)",
+    "Washer: Select your Spin Item (Minutes)",
+    "Washer: Select your Rinse Item (Minutes)",
+    "Washer: Select your Drain Item (Minutes)",
     "Washer: You didn't select any cylcle, please click cycle icon!"
 ]
 
@@ -271,8 +272,8 @@ dialog_group = pygame.sprite.LayeredDirty()
 
 MENU_COUNT = 5
 selectedItem = [-1 for i in range(MENU_COUNT)]
-
 cycle = [0 for i in range(MENU_COUNT)]
+itemText = [["0", "1", "2", "4", "8"],["0", "1", "2", "4", "8"]]
 
 menuSoundEffect = pygame.mixer.Sound('sounds/menu.wav')
 menuSoundEffect.set_volume(0.1)
@@ -297,6 +298,7 @@ while run:
 
 
     screen.blit(pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT)), (0, 0))
+
     item_group.update(event_list)
     item_group.draw(screen)
 
@@ -318,6 +320,7 @@ while run:
         drain_group.update(event_list)
         drain_group.draw(screen)
 
+    bar.render(screen)
     washer.render(screen)
 
     # if dialog_step <= FINAL_STEP:
@@ -326,7 +329,6 @@ while run:
     rects = dialog_group.draw(screen)
     pygame.display.update(rects)
 
-    bar.render(screen)
     pygame.display.flip()
 
 pygame.quit()
